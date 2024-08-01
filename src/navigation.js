98788d3420947fae0b74d1d2a50444bb6cb13306
bernatsampera <b.sampera@gmail.com>
2024-08-01 15:21:55 +0200

    feat: add image and adjust main text

--- src/navigation.js
+++ src/navigation.js
@@ -45,7 +45,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/basmpera/astromvp' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://bernatsampera.com/favicon/favicon-32x32.png" alt="bernatsampera logo" loading="lazy"></img>
     Made by <a class="text-blue-600 underline text-muted" href="https://bernatsampera.com/"> bernatsampera</a> · All rights reserved.
   `,
 };
