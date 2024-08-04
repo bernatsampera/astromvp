95a035b02849c3017453b801d99eb4bbc8b5d5f0
bernatsampera <b.sampera@gmail.com>
2024-08-04 15:53:39 +0200

    feat: remove tailwind extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,20 +5,7 @@ import daisyui from 'daisyui';
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      colors: {
-        primary: 'var(--aw-color-primary)',
-        secondary: 'var(--aw-color-secondary)',
-        accent: 'var(--aw-color-accent)',
-        default: 'var(--aw-color-text-default)',
-        muted: 'var(--aw-color-text-muted)',
-      },
-      fontFamily: {
-        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
-        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [typographyPlugin, daisyui],
   darkMode: 'class',
