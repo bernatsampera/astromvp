488b25382d1eefbfa69e9a3e2e6b1d958fdd0df5
bernatsampera <b.sampera@gmail.com>
2025-03-29 12:18:09 +0100

    feat: update astromvp with fonts and more

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
 import daisyui from 'daisyui';
 
@@ -6,9 +5,6 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
       colors: {
         black: '#111111',
         'off-white': '#f7f7f7',
